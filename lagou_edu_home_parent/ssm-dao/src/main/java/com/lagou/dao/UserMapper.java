9bbeb69de201b5c26a4dc25690c2a7a8da91c1b3
wilfred <724091702@qq.com>
2021-12-27 15:26:43 +0800

    update GitMapper

--- lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -49,10 +49,16 @@ public interface UserMapper {
         4.获取用户拥有的资源权限信息
      */
     public List<Resource> findResourceByRoleId(List<Integer> ids);
+
     public List<Resource> findResourceByRoleId2(List<Integer> ids);
 
-
-
+    public void test1();
+    public void test2();
+    public void test3();
+    public void test4();
+    public void test5();
+    public void test6();
+    public void test7();
 
 
 
