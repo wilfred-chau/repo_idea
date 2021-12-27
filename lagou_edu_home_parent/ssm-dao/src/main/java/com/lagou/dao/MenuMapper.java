9d6e67a6d3bd8e22e91e0869549e6eac9b348371
wilfred <724091702@qq.com>
2021-12-27 15:09:49 +0800

    first commit

---
+++ lagou_edu_home_parent/ssm-dao/src/main/java/com/lagou/dao/MenuMapper.java
@@ -0,0 +1,25 @@
+package com.lagou.dao;
+
+import com.lagou.domain.Menu;
+
+import java.util.List;
+
+public interface MenuMapper {
+
+    /*
+        查询所有父子菜单信息
+     */
+
+    public List<Menu> findSubMenuListByPid(int pid);
+
+    /*
+        查询所有菜单列表
+     */
+    public List<Menu> findAllMenu();
+
+
+    /*
+    根据id查询菜单信息
+     */
+    public Menu findMenuById(Integer id);
+}
---
+++ lagou_edu_home_parent/ssm-service/src/main/java/com/lagou/service/impl/RoleServiceImpl.java
@@ -0,0 +1,68 @@
+package com.lagou.service.impl;
+
+import com.lagou.dao.RoleMapper;
+import com.lagou.domain.Role;
+import com.lagou.domain.RoleMenuVo;
+import com.lagou.domain.Role_menu_relation;
+import com.lagou.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+
+    @Override
+    public List<Role> findAllRole(Role role) {
+        List<Role> allRole = roleMapper.findAllRole(role);
+        return allRole;
+    }
+
+    @Override
+    public List<Integer> findMenuByRoleId(Integer roleid) {
+        List<Integer> menuByRoleId = roleMapper.findMenuByRoleId(roleid);
+
+        return menuByRoleId;
+    }
+
+    @Override
+    public void roleContextMenu(RoleMenuVo roleMenuVo) {
+
+        // 1.清空中间表的关联关系
+        roleMapper.deleteRoleContextMenu(roleMenuVo.getRoleId());
+
+        // 2.为角色分配菜单
+        List<Integer> menuIdList = roleMenuVo.getMenuIdList();
+        for (Integer mid : menuIdList) {
+            Role_menu_relation role_menu_relation = new Role_menu_relation();
+            role_menu_relation.setMenuId(mid);
+            role_menu_relation.setRoleId(roleMenuVo.getRoleId());
+
+            // 封装数据
+            Date date = new Date();
+            role_menu_relation.setCreatedTime(date);
+            role_menu_relation.setUpdatedTime(date);
+            role_menu_relation.setCreatedBy("system");
+            role_menu_relation.setUpdatedby("system");
+            roleMapper.roleContextMenu(role_menu_relation);
+        }
+    }
+
+    /*
+    删除角色
+     */
+    @Override
+    public void deleteRole(Integer roleId) {
+
+        // 调用根据roleId清空中间表关联关系的方法
+        roleMapper.deleteRoleContextMenu(roleId);
+        // 删除
+        roleMapper.deleteRole(roleId);
+    }
+}
